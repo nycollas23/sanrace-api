4defe2b6381458cc04668e319d471083478e0ec6
Nycollas Eufrasio da Silva <nycollas.silva-ext@viavarejo.com.br>
2020-07-09 00:22:34 -0300

    App.Js

---
+++ app.js
@@ -0,0 +1,14 @@
+// Imports
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.listen('3001');
+require('./src/routing/pilot-routing')(app);
+
+console.info('Servidor Iniciando em 3001');
