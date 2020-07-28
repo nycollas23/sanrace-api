97eddd2c94180af3b67e87f7edaca288e29d0cd5
Nycollas Eufrasio da Silva <nycollaseuf@outlook.com>
2020-07-27 23:38:55 -0300

    Adição de Estoque

--- app.js
+++ app.js
@@ -14,5 +14,6 @@ require('./src/routing/auth.routing')(app);
 require('./src/routing/user.routing')(app);
 require('./src/routing/campeonato.routing')(app);
 require('./src/routing/dashboard.routing')(app);
+require('./src/routing/estoque.routing')(app);
 
 console.info('Servidor Iniciando em 3000');
