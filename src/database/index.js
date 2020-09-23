5eb2421b42e3cc69a30a2622aaaba3133b69f68e
Nycollas Eufrasio da Silva <nycollas.silva-ext@viavarejo.com.br>
2020-09-22 21:58:22 -0300

    Teste

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,11 @@ const DB = process.env.SR_MONGO_DB || 'sanrace';
 const USER = process.env.SR_MONGO_USER || '';
 const PWD = process.env.SR_MONGO_PWD || '';
 
-const dbPath = `mongodb://${USER}:${PWD}@${URL}:27017/${DB}`;
+// const dbPath = `mongodb://${USER}:${PWD}@${URL}:27017/${DB}`;
+
+const dbPath = "mongodb://sanrace01:S@nR@CE@localhost:27017/sanrace01";
+
+
 
 mongoose.connect(dbPath, {
     useNewUrlParser: true,
